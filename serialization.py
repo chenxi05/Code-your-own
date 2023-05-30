95585c9dab39a67d4b6cffbfb3c54b8562e18471
s-renam <amyrenhome@gmail.com>
2023-05-30 10:26:37 -0700

    serialization implementation

--- serialization.py
+++ serialization.py
@@ -1,2 +1,21 @@
 #serialzation file
 import pickle
+
+from os import path
+
+def save(object, file):
+    if path.exists(file):
+        with open(file, "wb") as f:
+            pickle.dump(object, f)
+
+def load(file):
+    if path.exists(file):
+        with open(file, "rb") as f:
+            return pickle.load(f)
+    return None
+
+
+USERS_FILE = "users.pkl"
+users = load(USERS_FILE)
+if users is None:
+    users = {}

2ae3720d32449b9cc4e1a44184ae6c2f2b165bed
chenxi05 <chenxibi05@gmail.com>
2023-06-02 10:27:48 -0700

    edit game

--- game.py
+++ game.py
@@ -12,29 +12,14 @@ trivia_questions = {
 }
 
 print("Welcome to our trivia game!")
-num_of_players=int(input("How many people are playing this game?"))
 
+user_name=input("Please enter your username: ") 
+pts=0
+for i in range(len(trivia_questions)):
+    print(trivia_questions.keys[i])
+#     if answer:
+#        pts += 1
+    #   else:
+    #      continue
 
-for i in num_of_players:
-    user_name=input("Please enter your username: ") 
-    pts=0
-    question1=input(trivia_questions.keys[0])
-    if question1==trivia_questions[trivia_questions.keys[0]]:
-        pts+=1
-    question2=input(trivia_questions.keys[1])
-    if question2==trivia_questions[trivia_questions.keys[1]]:
-        pts+=1
-    question3=input(trivia_questions.keys[2])
-    if question3==trivia_questions[trivia_questions.keys[2]]:
-        pts+=1
-    question4=input(trivia_questions.keys[3])
-    if question4==trivia_questions[trivia_questions.keys[3]]:
-        pts+=1
-    question5=input(trivia_questions.keys[4])
-    if question5==trivia_questions[trivia_questions.keys[4]]:
-        pts+=1
-    question6=input(trivia_questions.keys[5])
-    if question6==trivia_questions[trivia_questions.keys[5]]:
-        pts+=1
-    ser.save(user_name, pts)
     
